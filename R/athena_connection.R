38c42dc1246ba29b548d3a79d7b558fdde8254aa
Peter Reader-Harris <peter.reader-harris@justice.gov.uk>
2021-09-01 07:06:18 +0000

    extra comments

--- R/athena_connection.R
+++ R/athena_connection.R
@@ -1,6 +1,12 @@
 
 #' @include Rdbtools.R
 
+# This extends the AthenaConnection S4 class defined by noctua to
+# be a class we can use in MoJ. This has two effects, one is to add
+# a slot with extra details (MoJdetails) and the other is that it
+# lets us define a new set of methods for the MoJAthenaConnection
+# objects - these new methods can be calls to the AthenaConnection
+# methods (which work by inheritance) but with some pre-processing.
 setClass(
   "MoJAthenaConnection",
   contains="AthenaConnection",
@@ -13,7 +19,9 @@ connect_athena <- function(aws_region = "eu-west-1",
                            rstudio_conn_tab = FALSE
 ) {
 
+  # this removes expired credentials
   check_credentials()
+  # get the athena user id, needed for staging dir and temp db name
   user_id <- athena_user_id()
 
   # work out what your staging dir should be on the AP if unset
@@ -22,15 +30,19 @@ connect_athena <- function(aws_region = "eu-west-1",
   }
 
   # connect to athena
+  # returns an AthenaConnection object, see noctua docs for details
   con <- dbConnect(noctua::athena(),
                    region_name = aws_region,
                    s3_staging_dir = staging_dir,
                    rstudio_conn_tab = rstudio_conn_tab)
 
+  # this works out the temp db name from the user id
   temp_db_name <- get_database_name_from_userid(user_id)
 
-
+  # coerce the AthenaConnection object to be a MoJAthenaConnection object
+  # this just adds the slot MoJdetails, as defined in setClass above
   con <- as(con,"MoJAthenaConnection")
+  # then we can set the extra details we need in MoJ in the new slot
   con@MoJdetails$user_id <- user_id
   con@MoJdetails$temp_db_name <- temp_db_name
   con@MoJdetails$temp_db_exists <- NA # Don't know if the temp db exists yet
