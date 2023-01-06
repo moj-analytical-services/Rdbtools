6ec6aeec1e59fad9cacc40137363470b36177fff
peter.reader-harris@justice.gov.uk <peter.reader-harris@justice.gov.uk>
2023-01-06 12:36:58 +0000

    use paws assume_role_with_web_identity() to authenticate

--- R/athena_connection.R
+++ R/athena_connection.R
@@ -40,98 +40,38 @@ setClass(
 connect_athena <- function(aws_region = NULL,
                            staging_dir = NULL,
                            rstudio_conn_tab = FALSE,
-                           session_duration = 3600,
+                           session_duration = 3600L,
                            role_session_name = NULL,
-                           ...
-) {
+                           ...) {
 
-  if (is.null(aws_region)) aws_region <- get_region()
+  credentials <- get_athena_credentials(aws_region,
+                                        session_duration,
+                                        role_session_name)
 
-  aws_role_arn <- Sys.getenv('AWS_ROLE_ARN')
-  aws_web_identity_token_file <- Sys.getenv('AWS_WEB_IDENTITY_TOKEN_FILE')
+  user_id <- stringr::str_split(credentials$AssumedRoleUser$Arn, '/')[[1]][2]
 
-  if (nchar(aws_role_arn) > 0 & nchar(aws_web_identity_token_file) > 0) {
-
-    # Obtain the WebIdentity credentials
-    # ref: https://docs.aws.amazon.com/STS/latest/APIReference/API_AssumeRoleWithWebIdentity.html
-    # Set the arbitrary session name to user plus timestamp
-    user <- stringr::str_split(aws_role_arn, '/')[[1]][2]
-    if (is.null(role_session_name)) role_session_name <- glue::glue("{user}_{as.numeric(Sys.time())}")
-    query = glue::glue(
-      "https://sts.amazonaws.com/",
-      "?Action=AssumeRoleWithWebIdentity",
-      "&DurationSeconds={session_duration}",
-      "&RoleSessionName={role_session_name}",
-      "&RoleArn={aws_role_arn}",
-      "&WebIdentityToken={readr::read_file(aws_web_identity_token_file)}",
-      "&Version=2011-06-15"
-    )
-    response <- httr::POST(query)
-
-    if (!is.null(httr::content(response)$Error$Message)) rlang::abort(c("Something went wrong getting temporary credentials",
-                                                                        "*" = "The message from https://sts.amazonaws.com/ is:",
-                                                                        "i" = httr::content(response)$Error$Message))
-
-    credentials <- httr::content(response)$AssumeRoleWithWebIdentityResponse$AssumeRoleWithWebIdentityResult$Credentials
-    #temporary_authentication <- TRUE
-    authentication_expiry <- as.POSIXct(credentials$Expiration, origin = "1970-01-01", tz="UTC")
-
-
-    # Use the WebIdentity credentials to access AWS services
-    sts_svc <- paws::sts(
-      config = list(
-        credentials = list(
-          creds = list(
-            access_key_id = credentials$AccessKeyId,
-            secret_access_key = credentials$SecretAccessKey,
-            session_token = credentials$SessionToken
-          )
-        ),
-        region = aws_region
-      )
-    )
-    user_id <- sts_svc$get_caller_identity()$UserId
-
-    # work out what your staging dir should be on the AP if unset
-    if (is.null(staging_dir)) {
-      staging_dir = get_staging_dir_from_userid(user_id)
-    }
-
-    # connect to athena
-    # returns an AthenaConnection object, see noctua docs for details
-    con <- dbConnect(noctua::athena(),
-                     region_name = aws_region,
-                     s3_staging_dir = staging_dir,
-                     rstudio_conn_tab = rstudio_conn_tab,
-                     aws_access_key_id = credentials$AccessKeyId,
-                     aws_secret_access_key = credentials$SecretAccessKey,
-                     aws_session_token = credentials$SessionToken,
-                     ...)
-  } else {
-
-    # get the athena user id, needed for staging dir and temp db name
-    svc <- paws::sts(config=list(region=aws_region))
-    user_id <- svc$get_caller_identity()$UserId
-    #temporary_authentication <- FALSE
-    authentication_expiry <- NULL
-    role_session_name <- NULL
-
-    # work out what your staging dir should be on the AP if unset
-    if (is.null(staging_dir)) {
-      staging_dir = get_staging_dir_from_userid(user_id)
-    }
-
-    # connect to athena
-    # returns an AthenaConnection object, see noctua docs for details
-    con <- dbConnect(noctua::athena(),
-                     region_name = aws_region,
-                     s3_staging_dir = staging_dir,
-                     rstudio_conn_tab = rstudio_conn_tab)
-
+  # work out what your staging dir should be on the AP if unset
+  if (is.null(staging_dir)) {
+    staging_dir = get_staging_dir_from_userid(user_id)
   }
+
+  # connect to athena
+  # returns an AthenaConnection object, see noctua docs for details
+  con <- dbConnect(noctua::athena(),
+                   region_name = aws_region,
+                   s3_staging_dir = staging_dir,
+                   rstudio_conn_tab = rstudio_conn_tab,
+                   aws_access_key_id = credentials$Credentials$AccessKeyId,
+                   aws_secret_access_key = credentials$Credentials$SecretAccessKey,
+                   aws_session_token = credentials$Credentials$SessionToken,
+                   ...)
+
 
   # this works out the temp db name from the user id
   temp_db_name <- get_database_name_from_userid(user_id)
+
+  # get the authentication expiry
+  authentication_expiry <- as.POSIXct(credentials$Credentials$Expiration, origin = "1970-01-01", tz="UTC")
 
   # coerce the AthenaConnection object to be a MoJAthenaConnection object
   # this just adds the slot MoJdetails, as defined in setClass above
--- R/credentials.R
+++ R/credentials.R
@@ -1,4 +1,46 @@
 
+get_athena_credentials <- function(aws_region = NULL,
+                                   session_duration = 3600L,
+                                   role_session_name = NULL) {
+
+  if (is.null(aws_region)) aws_region <- get_region()
+
+  # Obtain the WebIdentity credentials
+  # ref: https://docs.aws.amazon.com/STS/latest/APIReference/API_AssumeRoleWithWebIdentity.html
+
+  aws_role_arn <- Sys.getenv('AWS_ROLE_ARN')
+  if (!(nchar(aws_role_arn) > 0)) rlang::abort(c("Could not find your AWS ARN", "i" = "The env variable is missing"))
+  aws_web_identity_token_file <- Sys.getenv('AWS_WEB_IDENTITY_TOKEN_FILE')
+  if (!(nchar(aws_web_identity_token_file) > 0)) rlang::abort(c("Could not find your token file path", "*" = "The env variable is missing"))
+
+  # Set the arbitrary session name to user plus timestamp
+  user <- stringr::str_split(aws_role_arn, '/')[[1]][2]
+  if (is.null(role_session_name)) role_session_name <- glue::glue("{user}_{as.numeric(Sys.time())}")
+
+  tryCatch({
+    # https://paws-r.github.io/docs/sts/
+    svc <- paws::sts(config = list(credentials = list(creds = list(access_key_id = "dummy",
+                                                                   secret_access_key = "dummy")), # see https://github.com/paws-r/paws/issues/512
+                                   region = get_region()))
+
+    creds <- svc$assume_role_with_web_identity(
+      DurationSeconds = session_duration,
+      RoleArn = aws_role_arn,
+      RoleSessionName = role_session_name,
+      WebIdentityToken = readr::read_file(aws_web_identity_token_file)
+    )},
+    error = function(e){
+      rlang::abort(c("Something went wrong getting temporary credentials",
+                     "*" = e))
+    }
+  )
+
+  return(creds)
+}
+
+
+
+
 is_auth_within_expiry <- function(con, window = 5 * 60) {
   expiry_t <- con@MoJdetails$authentication_expiry
   ifelse(
