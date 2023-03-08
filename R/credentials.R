8a642cc70a0719e50fa2aa221762160bd4a17f8d
ymao2 <yikang.mao@digital.justice.gov.uk>
2023-03-08 19:49:44 +0000

    Added some notes for the region env vars

--- R/credentials.R
+++ R/credentials.R
@@ -8,8 +8,15 @@ is_auth_within_expiry <- function(con, window = 5 * 60) {
   )
 }
 
-# Check for region in environment variables, otherwise use 'eu-west-1'
-# as the default
+# Check for region in environment variables based on the following order.
+# AWS_ATHENA_QUERY_REGION: 
+#   An environment variable for specifying the region when the region 
+#   where the query will be run is different from the default region 
+#   from underlying running environment.
+# AWS_DEFAULT_REGION and AWS_REGION:
+#   The default region. Usually the 2 variables will be setup by the
+#   underlying running environment e.g. cluster, and they cannot be amended
+# othewise use 'eu-west-1' as the default
 get_region <- function() {
   if (nchar(Sys.getenv("AWS_ATHENA_QUERY_REGION")) > 0) {
     return(Sys.getenv("AWS_ATHENA_QUERY_REGION"))
